555064be44fe6bb612bec73e73154e9253837fc5
nuintun <nuintun@qq.com>
2022-04-11 17:51:32 +0800

    ⭕ Remove ok log styles

--- tests/src/index.jsx
+++ tests/src/index.jsx
@@ -14,9 +14,5 @@ if (import.meta.webpackHot) {
 }
 
 on('ok', ({ builtAt }) => {
-  console.info(
-    `%c⭕%c[HMR]: App is up to date at ${new Date(builtAt).toLocaleString()}`,
-    'color: #f92f60; font-weight: normal;',
-    'color: #1890ff; font-weight: bold;'
-  );
+  console.log(`[HMR] App is up to date at ${new Date(builtAt).toLocaleString()}`);
 });
