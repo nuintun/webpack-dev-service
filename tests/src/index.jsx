a0e6c86cb5c83d1adee3c25a6614759a2d911fd0
nuintun <nuintun@qq.com>
2022-04-11 17:06:04 +0800

    ✨ Update tests

--- tests/src/index.jsx
+++ tests/src/index.jsx
@@ -14,5 +14,9 @@ if (import.meta.webpackHot) {
 }
 
 on('ok', ({ builtAt }) => {
-  console.info(`📌 %c[HMR]: App is up to date at ${new Date(builtAt).toLocaleString()}.`, `color: #099160;`);
+  console.info(
+    `%c⭕%c[HMR]: App is up to date at ${new Date(builtAt).toLocaleString()}`,
+    'color: #f92f60; font-weight: normal;',
+    'color: #1890ff; font-weight: bold;'
+  );
 });
