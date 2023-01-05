1308fa59612ef36d133288a01b39ef59e506f46a
nuintun <nuintun@qq.com>
2023-01-05 20:45:13 +0800

    feat: Update ANSI

--- src/client/ui/utils/ansi/enum.ts
+++ src/client/ui/utils/ansi/enum.ts
@@ -3,11 +3,11 @@
  */
 
 export const enum TokenType {
-  EOS, // End of Operating System
+  EOS, // End Operating System
   ESC, // A single ESC char - random
   OSC, // Operating System Command
   SGR, // Select Graphic Rendition
-  TEXT, // ANSI normal text
+  TEXT, // ANSI normal text group
   INCESC, // An Incomplete ESC sequence
   UNKNOWN // A valid CSI but not an SGR code
 }
